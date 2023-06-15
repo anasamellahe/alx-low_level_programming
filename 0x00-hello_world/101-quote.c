b4ce57a60e61025dc4f4d42e28101cb9b6c88193
anas <amellaheanas@gmail.com>
2023-06-15 18:46:04 +0100

    fix_2_101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,11 +2,11 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 58);
 	write(1, "\n", 1);
-	return (0);
+	return (1);
 }
