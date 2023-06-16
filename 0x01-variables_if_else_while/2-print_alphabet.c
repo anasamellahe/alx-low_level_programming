8483caff2fd22e7f77bf90cc6f34fde2e0f10129
anas <amellaheanas@gmail.com>
2023-06-16 23:45:57 +0100

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+/**
+ * main - print alphabet
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	for (int i = 0; i <= 10; i++)
+		write(1, &"0123456789\n"[i], 1);
+	return (0);
+}
