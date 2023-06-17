d0f02b079c4340dcfb4e8b9baf32547f057dba7f
anas <amellaheanas@gmail.com>
2023-06-17 03:05:42 +0100

    9-print_comb.c

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print com
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+		if (i != '9')
+			putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
