fee9cd72437fef95ec6930f27b47de1b72c034fc
anas <amellaheanas@gmail.com>
2023-06-20 23:53:25 +0100

    6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _abs - comutes the absolute value of ana integer
+ * @j: number
+ * Return: Returns the absolute value of the integer argument
+ */
+int _abs(int j)
+{
+	if (j < 0)
+		return (j * -1);
+	return (j);
+}
