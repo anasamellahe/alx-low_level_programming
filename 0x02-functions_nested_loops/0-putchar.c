a77e3738ade1adaea443d9ab0d1764baad478617
anas <amellaheanas@gmail.com>
2023-06-20 07:45:48 +0100

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <unistd.h>
+/**
+ * main - starting point
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	char *string  = "_putchar";
+
+	for (int i = 0; string[i]; i++)
+		_putchar(string[i];
+	return (0);
+}
