f7dfbabf30818b43873f08135995826ae7bb20c1
anas <amellaheanas@gmail.com>
2023-06-26 11:34:38 +0100

    1-swap.c

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,8 +1,8 @@
 /**
-* swa_int - swaps the values of two integers.
-* @a : pointer a
-* @b : pointer b
-*/
+ * swap_int - swaps the values of two integers.
+ * @a : pointer a
+ * @b : pointer b
+ */
 void swap_int(int *a, int *b)
 {
 	int swap;
