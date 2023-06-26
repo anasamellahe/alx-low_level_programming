64cb7560507203b19c1bd96a0a29a8b735ab4d9f
anas <amellaheanas@gmail.com>
2023-06-27 00:57:46 +0100

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,28 +1,5 @@
 #include "main.h"
 /**
- * ft_isspace - check if space
- * @c: charater to check
- * Return: 0 if c is not space , 1 if c space
- */
-int ft_isspace(int c)
-{
-	if (c == ' ' || c == '\n' || c == '\t' || c == '\v' ||
-		c == '\f' || c == '\r')
-	return (1);
-	return (0);
-}
-/**
- * ft_isnumber - check if number
- * @c: charater to check
- * Return: 0 if c is not number , 1 if c number
- */
-int ft_isnumber(int c)
-{
-	if (c >= '0' && c <= '9')
-		return (1);
-	return (0);
-}
-/**
  * _atoi - convert a string to an integer
  * @s: pointer
  * Return: number
@@ -31,23 +8,23 @@ int _atoi(char *s)
 {
 	int i;
 	int sign;
-	int number;
+	unsigned int number;
 
 	i = 0;
 	sign = 1;
 	number = 0;
-	while (!ft_isnumber((int)s[i]))
+	while (!(s[i] >= '0' && s[i] <= '9') && s[i])
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		i++;
 	}
-	while (ft_isnumber((int)s[i]))
+	while ((s[i] >= '0' && s[i] <= '9') && s[i])
 	{
 		number = number * 10 + (s[i] - 48);
 		i++;
 	}
-	return (number * sign);
+	return ((int)(number * sign));
 }
 
 
