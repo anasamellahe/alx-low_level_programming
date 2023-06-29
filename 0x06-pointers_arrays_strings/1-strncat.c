8add415d6d474f8ce626f39d893a3d576cf91ede
anas <amellaheanas@gmail.com>
2023-06-29 14:41:22 +0100

    1-strncat.c

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strncat -  appends n bytes from src string to the dest string
+ * @dest: the string pointedto by 'dest'
+ * @src: the string pointedto by 'src'
+ * Return: a pointer  to  the  resulting string dest.
+ */
+char *_strncat(char *dest, char *src)
+{
+	int i;
+	unsigned int len;
+
+	i = 0;
+	len = strlen(dest);
+	while (src[i] && i < n)
+	{
+		dest[len + i] = src[i];
+		i++;
+	}
+	dest[len + i] = '\0';
+	return (dest);
+}
