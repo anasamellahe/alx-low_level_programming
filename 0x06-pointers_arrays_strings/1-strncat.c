66c24cddf451e43f368d00d3bc685889e3aa2ed6
anas <amellaheanas@gmail.com>
2023-06-29 14:44:58 +0100

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,9 +4,10 @@
  * _strncat -  appends n bytes from src string to the dest string
  * @dest: the string pointedto by 'dest'
  * @src: the string pointedto by 'src'
+ * @n: number of  bytes from src
  * Return: a pointer  to  the  resulting string dest.
  */
-char *_strncat(char *dest, char *src)
+char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 	unsigned int len;
