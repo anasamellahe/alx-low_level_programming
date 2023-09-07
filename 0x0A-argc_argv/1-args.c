7bc41c2b2323883481ac5830a2066540828b573a
anas <amellaheanas@gmail.com>
2023-09-07 20:27:14 +0100

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - main function
+ * @ac: arg count
+ * @av: array of arg
+ * Return: 0 Success
+ */
+
+int main(int ac, char *av[])
+{
+	printf("%d\n", ac - 1);
+	return (0);
+}
+
