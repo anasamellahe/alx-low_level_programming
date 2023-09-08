2e8038fd206505c3c328f52745c162ffe9bf525c
anas <amellaheanas@gmail.com>
2023-09-08 22:35:52 +0100

    100-realloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -7,5 +7,6 @@ char *_strncat(char *dest, char *src, unsigned int n);
 int _strlen(char *str);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 #endif
 
