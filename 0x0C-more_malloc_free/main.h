b8eef5d4458372c694cb37dd4e9bea2967ae64bf
anas <amellaheanas@gmail.com>
2023-09-08 22:31:38 +0100

    2-calloc.c

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -5,5 +5,6 @@ void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 char *_strncat(char *dest, char *src, unsigned int n);
 int _strlen(char *str);
+void *_calloc(unsigned int nmemb, unsigned int size);
 #endif
 
