dd2b0f60fb3556ca9b0a08eae9009da7cb734c9c
anas <amellaheanas@gmail.com>
2023-09-12 03:58:26 +0100

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -46,25 +46,29 @@ void _strcpy(char *dest, char *src)
  * Return: the variable already initialized
  */
 
-dog_t *new_dog(char *name, float age, char *owner)
+dog_t	*new_dog(char *name, float age, char *owner)
 {
 	dog_t *new_dog;
 
 	new_dog = malloc(sizeof(dog_t));
 	if (!new_dog)
 		return (NULL);
-	new_dog->name = malloc((_strlen(name) + 1) * sizeof(char));
-	new_dog->owner = malloc((_strlen(owner) + 1) * sizeof(char));
-	if (new_dog->owner == NULL || new_dog->name == NULL)
+	new_dog->name = malloc(sizeof(char) * (_strlen(name) + 1));
+	if (!new_dog->name)
 	{
-		return (NULL);
-		free (new_dog->name);
-		free(new_dog->owner);
 		free(new_dog);
+		return (NULL);
 	}
 	_strcpy(new_dog->name, name);
-	new_dog->age = age;
+	new_dog->owner = malloc(sizeof(char) * (_strlen(owner) + 1));
+	if (!new_dog->owner)
+	{
+		free(new_dog->name);
+		free(new_dog);
+		return (NULL);
+	}
 	_strcpy(new_dog->owner, owner);
+	new_dog->age = age;
 	return (new_dog);
 }
 
