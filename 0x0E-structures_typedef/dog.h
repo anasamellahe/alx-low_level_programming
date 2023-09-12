bba08fd41bd184cb18bd5ed5394be4459a4059eb
anas <amellaheanas@gmail.com>
2023-09-12 04:02:57 +0100

    dog.h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -23,5 +23,6 @@ void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void _strcpy(char *dest, char *src);
 int _strlen(char *s);
+void    free_dog(dog_t *d);
 #endif
 
