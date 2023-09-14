92141a17f2ba5089e593162d79a6dab5cd60743a
anas <amellaheanas@gmail.com>
2023-09-14 04:58:50 +0100

    3-main.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,6 +1,6 @@
 #include "3-calc.h"
-#include <string.h>
 #include <stdio.h>
+#include <stddef.h>
 /**
  * get_op_func - selects the correct function to perform the operation asked by
  * the user
@@ -23,7 +23,7 @@ int (*get_op_func(char *s))(int, int)
 	int	i;
 
 	i = 0;
-	while (*(ops[i].op) != *s && ops[i].op)
+	while (ops[i].op && *(ops[i].op) != *s)
 		i++;
 	return (ops[i].f);
 }
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -19,27 +19,27 @@ int main(int ac, char *av[])
 	int	resoult;
 	int	(*op)(int a, int b);
 
-	if (ac == 4)
+	if (ac != 4)
 	{
-		if ((av[2][0] ==  '/' || av[2][0] == '%') &&
-				av[3][0] == '0')
-		{
-			printf("Error\n");
-			exit(100);
-		}
-		a = atoi(av[1]);
-		b = atoi(av[3]);
-		op = get_op_func(av[2]);
-		if (op == NULL || av[2][1] != '\0')
-		{
-			printf("Error\n");
-			exit(99);
-		}
-		resoult = op(a, b);
-		printf("%d\n", resoult);
-		return (0);
+		printf("Error\n");
+		exit(98);
 	}
-	printf("Error\n");
-	exit(98);
+	if ((av[2][0] ==  '/' || av[2][0] == '%') &&
+			av[3][0] == '0')
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	a = atoi(av[1]);
+	b = atoi(av[3]);
+	op = get_op_func(av[2]);
+	if (op == NULL || av[2][1])
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	resoult = op(a, b);
+	printf("%d\n", resoult);
+	return (0);
 }
 
