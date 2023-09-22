484249bdff7adeb01aabb24125c63dd76ee4e628
anas <amellaheanas@gmail.com>
2023-09-22 18:38:14 +0100

    first

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+* first - print befor main;
+*/
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\nI ");
+	printf("bore my house upon my back!\n");
+}
